12feb8a3b07ee12b11e6c440cc1f80e711c69604
Your Name <you@example.com>
2019-08-07 13:45:31 +0800

    会员专享 周边游

---
+++ components/productCard/card.js
@@ -0,0 +1,28 @@
+// components/productCard/card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    cardData: { type: [Object, Array] },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    // img: "cloud://hy-2ecbc6.6879-hy-2ecbc6/Aladdin/goods.png",
+    // place : "福建",
+    // title: "福州/武夷山/鼓浪屿/厦门/北海/桂林/阳朔3飞11日屿/厦门/北海/桂林/阳朔3飞11日",
+    // toast: "经典纯玩团0购物私家团让您满意而归经典纯玩团0购物私家团让您满意而归",
+    // keyList:["爆款","爆款","爆款"],
+    // num : "170"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  },
+})
--- pages/home/travelAround/travelAround.js
+++ pages/home/travelAround/travelAround.js
@@ -5,62 +5,117 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    headImg: "cloud://hy-2ecbc6.6879-hy-2ecbc6/Aladdin/travelHead.png",
+    travelList: [{
+      id: 1,
+      title: "概况",
+      itemList: [{
+        msg: "一张图玩遍哈尔滨"
+      }, {
+        msg: "必打卡景点"
+      }, {
+        msg: "去二龙山看日出"
+      }, {
+        msg: "热门旅游路线"
+      }]
+    }, {
+      id:2,
+        title: "娱乐",
+        itemList: [{
+          msg: "冰城特色"
+        }, {
+            msg: "热门电影"
+        }, {
+            msg: "德云社相声"
+        }]
+      }, {
+        id: 3,
+        title: "美食",
+        itemList: [{
+          msg: "冰城美食"
+        }, {
+            msg: "餐厅推荐"
+        }]
+      }, {
+        id: 4,
+        title: "交通",
+        itemList: [{
+          msg: "地铁"
+        }, {
+            msg: "公交"
+        }, {
+            msg: "出租"
+        }]
+      }, {
+        id: 5,
+        title: "购物",
+        itemList: [{
+          msg: "哈尔滨购物指南"
+        }]
+      }, {
+        id: 6,
+        title: "住宿",
+        itemList: [{
+          msg: "哈尔滨住宿指南"
+        }, {
+            msg: "热门住宿区域"
+        }]
+      }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
