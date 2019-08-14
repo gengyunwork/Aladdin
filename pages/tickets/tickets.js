786c5054a8605e1583f93f7545c47ff446c1dc7a
DengSuning <deng_suning@163.com>
2019-08-14 17:05:11 +0800

    布局：票单 疯狂砍价兑换

--- pages/tickets/tickets.js
+++ pages/tickets/tickets.js
@@ -5,62 +5,104 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tickets: [{
+        id: 0,
+        title: "二龙山完美一日游",
+        desc: "二龙山是首批国家AAAA级旅游区，素有“哈尔滨东花园”之称，位于哈尔滨市东50公里处",
+        location: "黑龙江省哈尔滨市古城宾县西南",
+        imgurl: "",
+        date: "2019-03-06",
+        time: "19:54",
+        num: 2,
+        state: 1
+      },
+      {
+        id: 1,
+        title: "二龙山完美一日游",
+        desc: "二龙山是首批国家AAAA级旅游区，素有“哈尔滨东花园”之称，位于哈尔滨市东50公里处",
+        location: "黑龙江省哈尔滨市古城宾县西南",
+        imgurl: "",
+        date: "2019-03-06",
+        time: "19:54",
+        num: 2,
+        state: 0
+      }, {
+        id: 2,
+        title: "二龙山完美一日游",
+        desc: "二龙山是首批国家AAAA级旅游区，素有“哈尔滨东花园”之称，位于哈尔滨市东50公里处",
+        location: "黑龙江省哈尔滨市古城宾县西南",
+        imgurl: "",
+        date: "2019-03-06",
+        time: "19:54",
+        num: 2,
+        state: 0
+      }, {
+        id: 3,
+        title: "二龙山完美一日游",
+        desc: "二龙山是首批国家AAAA级旅游区，素有“哈尔滨东花园”之称，位于哈尔滨市东50公里处",
+        location: "黑龙江省哈尔滨市古城宾县西南",
+        imgurl: "",
+        date: "2019-03-06",
+        time: "19:54",
+        num: 2,
+        state: 0
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
